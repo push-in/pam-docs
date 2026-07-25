298ba0df7406ddc72221174d39f3b4e00c225c93
davidbalbino <davidwilliambalbino@gmail.com>
2026-07-25 12:52:40 -0300

    fix: bundle server runtime dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,11 @@ export default defineConfig({
   adapter: node({
     mode: 'standalone',
   }),
+  vite: {
+    ssr: {
+      noExternal: true,
+    },
+  },
   integrations: [
     starlight({
       title: 'PAM',
